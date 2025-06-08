ec62e24be94ad1d705ca4e2b7fe8ef6cb34415f9
Sky <universitycluster1.1@gmail.com>
2025-06-08 21:59:22 +0530

    all assignment of week 1 done

--- Week 1/Problem Statement 02/Solution.py
+++ Week 1/Problem Statement 02/Solution.py
@@ -1,21 +1,3 @@
-def minion_game(string):
-    vowels = set('AEIOU')
-    kevin_score = 0
-    stuart_score = 0
-    n = len(string)
-
-    for i, ch in enumerate(s):
-        if ch in vowels:
-            kevin_score += n - i
-        else:
-            stuart_score += n - i
-
-    if kevin_score > stuart_score:
-        print("Kevin", kevin_score)
-    elif stuart_score > kevin_score:
-        print("Stuart", stuart_score)
-    else:
-        print("Draw")
-
-
-if __name__ == '__main__':
+from itertools import groupby
+for key, group in groupby(input()):
+    print('({}, {})'.format(len(list(group)), key), end = " ")
